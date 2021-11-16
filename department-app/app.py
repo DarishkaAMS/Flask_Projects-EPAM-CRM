2c141a87535886c508612c6c9f2c69a940915641
aceWmJRN <d.nikolaienko@yahoo.co.uk>
2021-11-16 07:21:12 +0200

    Add .env and update DB

--- department-app/app.py
+++ department-app/app.py
@@ -5,20 +5,17 @@ from dotenv import load_dotenv
 import os
 
 app = Flask(__name__, instance_relative_config=True)
-# app.config.from_pyfile('config.py')
 
 ENV = 'dev'
 
-# load_dotenv()
-# SQLALCHEMY_DATABASE_URI = os.getenv('DB_CREDENTIALS')
-# print('DB_CREDENTIALS', DB_CREDENTIALS)
-# print('SQLALCHEMY_DATABASE_URI', SQLALCHEMY_DATABASE_URI)
+load_dotenv()
 
 if ENV == 'dev':
     app.debug = True
     app.secret_key = 'dev'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_CREDENTIALS')}"
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_CREDENTIALS')}"
+    # print("FFF", f"postgresql://{os.getenv('DB_CREDENTIALS')}")
+    # print("FFF", app.config['SQLALCHEMY_DATABASE_URI'] == f"postgresql://{os.getenv('DB_CREDENTIALS')}")
 
 else:
     app.debug = False
@@ -33,7 +30,7 @@ class Employee(db.Model):
     __tablename__ = 'employees'
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(200))
-    last_name = db.Column(db.String(200), unique=True)
+    last_name = db.Column(db.String(200))
     department = db.Column(db.String(25))
     salary = db.Column(db.Integer)
     rating = db.Column(db.Integer)
@@ -62,17 +59,21 @@ def submit():
         salary = request.form['salary']
         rating = request.form['rating']
         comments = request.form['comments']
+        print("SUBMIT")
 
         if first_name == '' or last_name == '' or department == '':
             return render_template('index.html', message='Please enter required fields')
-        if db.session.query(Employee).filter(Employee.last_name == last_name).count() == 0:
-            data = Employee(first_name, last_name, department, salary, rating, comments)
-            db.session.add(data)
-            db.session.commit()
-            print("SEND")
+        # if db.session.query(Employee).filter(Employee.last_name == last_name).count() == 0 or \
+        #         db.session.query(Employee).filter(Employee.first_name == first_name).count() == 0:
+        data = Employee(first_name, last_name, department, salary, rating, comments)
+        db.session.add(data)
+        db.session.commit()
+        print("SEND")
             # send_mail(customer, dealer, rating, comments)
             # return render_template('success.html')
-        return render_template('index.html', message='Your department data has been saved')
+        # else:
+        #     return render_template('index.html', message='We already have this employee in our database')
+        return render_template('success.html', message='Your department data has been saved')
 
 
 if __name__ == "__main__":
