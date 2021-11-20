bcb13e7bd9aacb1feb925bba8945c56959d6eb7f
aceWmJRN <d.nikolaienko@yahoo.co.uk>
2021-11-20 09:49:37 +0200

    Add Department & Employee classes

--- Department_app/users.py
+++ Department_app/users.py
@@ -6,14 +6,16 @@ class Department(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(25))
     head = db.Column(db.String(25))
+    employee = db.relationship('Employee')
 
 
-class User(db.Model, UserMixin):
+class Employee (db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(25))
     last_name = db.Column(db.String(25))
-    # date_of_birth = db.Column(db.String(25))
+    date_of_birth = db.Column(db.DateTime(timezone=True))
     email = db.Column(db.String(50), unique=True)
     department = db.Column(db.String(15))
     salary = db.Column(db.Integer)
     password = db.Column(db.String(25))
+    department_id = db.Column(db.Integer, db.ForeignKey('department.id'))
