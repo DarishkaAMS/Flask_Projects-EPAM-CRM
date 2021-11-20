2160195d614b0864ffaabd2a3321f0f740c02c25
aceWmJRN <d.nikolaienko@yahoo.co.uk>
2021-11-20 06:54:02 +0200

    Add flash messages

--- Department_app/views/auth.py
+++ Department_app/views/auth.py
@@ -1,11 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, flash, render_template, request
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', text="TESTING")
+    return render_template('login.html')
 
 
 @auth.route('/logout')
@@ -13,11 +13,28 @@ def logout():
     return '<h2> LOGOUT </h2>'
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == "POST":
+        first_name = request.form.get('firstName')
+        last_name = request.form.get('lastName')
+        email = request.form.get('email')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(first_name) < 2:
+            flash('First name must be greater than 2 characters.', category='error')
+        elif len(last_name) < 2:
+            flash('Last name must be greater than 2 characters.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(password1) < 8:
+            flash('Password must be at least 8 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        else:
+            flash('Account has been creates', category='success')
+
     return render_template('sign_up.html')
 
 
-@auth.route('/new')
-def new():
-    return render_template('create_employee.html')
