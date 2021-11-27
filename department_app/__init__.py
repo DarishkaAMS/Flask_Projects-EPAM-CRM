fa7cf3254a453e487a9895aafb26328015f0ea43
aceWmJRN <d.nikolaienko@yahoo.co.uk>
2021-11-27 11:19:01 +0200

    Add create_app & create_database

--- department_app/__init__.py
+++ department_app/__init__.py
@@ -6,32 +6,30 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 
+from dotenv import load_dotenv
 from config import Config
+from os import path
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 bcrypt = Bcrypt()
+migrate = Migrate()
 
 
 def create_app():
     """
     Create flask application
-    :return: the created flask application
+    :return: the app instance
     """
     app = Flask(__name__)
     app.config.from_object(Config)
-
     db.init_app(app)
-
     bcrypt.init_app(app)
-
     login_manager.init_app(app)
-
-    Migrate(app, db)
+    migrate.init_app(app, db)
 
     from .views import user as user_blueprint
     app.register_blueprint(user_blueprint)
-
     from .rest import employee_api, department_api
 
     api = Api(app)
@@ -44,7 +42,18 @@ def create_app():
     api.add_resource(employee_api.EmployeeListApi, '/api/employees')
     api.add_resource(employee_api.Employee, '/api/employees/<id>')
 
-    from . import models
+    from .models.department import Department
+    from .models.employee import Employee
+
+    create_database(app)
+    print('CREATE EXISTS!', db)
 
     return app
 
+
+def create_database(app):
+    # if not path.exists('website/' + DB_NAME):
+    #     db.create_all(app=app)
+    print('Created EXISTS!', db)
+    db.create_all(app=app)
+    print('Created Database!', db)
--- run.py
+++ run.py
@@ -1,6 +1,8 @@
-from Department_app import create_app
+from department_app import create_app, db
 
 app = create_app()
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    # app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True)
