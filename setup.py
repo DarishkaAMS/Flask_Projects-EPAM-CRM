f6ff5c6c1af7c844e49fd892e0a4dace98ed0395
aceWmJRN <d.nikolaienko@yahoo.co.uk>
2021-12-04 15:27:23 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xworker',
+    version='1.0',
+    description='Python Distribution Utilities',
+    author='Daryna Nikolaienko',
+    author_email='d.nikolaienko@yahoo.co.uk',
+    packages=find_packages(),
+)
