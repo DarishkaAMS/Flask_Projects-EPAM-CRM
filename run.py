24e64d4313752b738e8a50a0d2c782ed196fcdfe
aceWmJRN <d.nikolaienko@yahoo.co.uk>
2021-12-13 06:11:01 +0200

    Update logs_rec

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ app = create_app()
 
 if __name__ == "__main__":
     import logging
-    logging.basicConfig(filename='error.log', level=logging.DEBUG,
+    logging.basicConfig(filename='logs_rec.log', level=logging.DEBUG,
                         format='%(levelname)s %(name)s %(threadName)s : %(message)s @ %(asctime)s')
 
     # app.run(host='0.0.0.0', debug=True)
